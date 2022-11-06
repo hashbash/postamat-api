c8b3c86045f8cd721fbd8d9aa277a50a65cd7fc6
u_m11r9 <egolubov@alfabank.ru>
2022-11-06 11:02:02 +0300

    add response model

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from fastapi import FastAPI
 from database import engine
+from models import ModelOutputResponse
 
 
 app = FastAPI()
@@ -11,7 +12,7 @@ def read_root():
     return {"Status": "Ok"}
 
 
-@app.get('/report', description='Return Model predictions')
+@app.get('/report', description='Return Model predictions', response_model=ModelOutputResponse)
 def get_model_predictions():
     df = pd.read_sql_query('select geo_h3_10, predictions, model_type from postamat.platform_model limit 1000',
                            con=engine)
