d20a5609e589de5d06e7c9c15f6a784bddb3d445
u_m11r9 <egolubov@alfabank.ru>
2022-11-06 11:04:53 +0300

    add response model

--- app/app.py
+++ app/app.py
@@ -12,8 +12,10 @@ def read_root():
     return {"Status": "Ok"}
 
 
-@app.get('/report', description='Return Model predictions', response_model=ModelOutputResponse)
+@app.get('/report', description='Return Model predictions',
+         # response_model=ModelOutputResponse
+         )
 def get_model_predictions():
     df = pd.read_sql_query('select geo_h3_10, predictions, model_type from postamat.platform_model limit 1000',
                            con=engine)
-    return df.to_json()
+    return df.to_dict('records')
